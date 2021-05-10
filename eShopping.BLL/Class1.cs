5719f5980fa6109ba52597d532870f600efc3599
PhiLong21 <vipvigame@gmail.com>
2021-05-10 16:33:28 +0700

    push model code first

--- eShopping.BLL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace eShopping.BLL
-{
-    public class Class1
-    {
-    }
-}
---
+++ eShopping.DAL/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopping.DAL.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
