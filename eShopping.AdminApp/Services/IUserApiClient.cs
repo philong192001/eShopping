33a9f272a26f8c4cb76252ab6c2358f1a6697b1c
PhiLong21 <vipvigame@gmail.com>
2021-05-18 16:26:49 +0700

    cookie authentication

---
+++ eShopping.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using eShopping.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopping.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
--- eShopping.BackendApi/Controllers/UsersController.cs
+++ eShopping.BackendApi/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ namespace eShopping.BackendApi.Controllers
 
         [HttpPost("authenticate")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
         {
             if (!ModelState.IsValid)
             {
@@ -32,12 +32,12 @@ namespace eShopping.BackendApi.Controllers
             if (string.IsNullOrEmpty(resultToken)){
                 return BadRequest("UserName or password is not correct");
             };
-            return Ok(new { token = resultToken });
+            return Ok( resultToken );
         }
 
         [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             if (!ModelState.IsValid)
             {
